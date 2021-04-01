27b12b88a3cfe65948a0d81f18dc40a535a0f499
croquelois <croquelois.adrien@gmail.com>
2021-04-01 23:22:03 +0200

    :passport_control: add firebase auth

---
+++ credentials.js
@@ -0,0 +1,74 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCuon7WUdd609Y85oBJJbD8ul3eGoO8RDs",
+  authDomain: "flutter-flip-card.firebaseapp.com",
+  databaseURL: "https://flutter-flip-card.firebaseio.com",
+  projectId: "flutter-flip-card",
+  storageBucket: "flutter-flip-card.appspot.com",
+  messagingSenderId: "186673725150",
+  appId: "1:186673725150:web:2e45457bb4499811f30c4f",
+  measurementId: "G-K5QTK9Y7PD"
+};
+firebase.initializeApp(config);
+
+/**
+ * initApp handles setting up the Firebase context and registering
+ * callbacks for the auth status.
+ *
+ * The core initialization is in firebase.App - this is the glue class
+ * which stores configuration. We provide an app name here to allow
+ * distinguishing multiple app instances.
+ *
+ * This method also registers a listener with firebase.auth().onAuthStateChanged.
+ * This listener is called when the user is signed in or out, and that
+ * is where we update the UI.
+ *
+ * When signed in, we also authenticate to the Firebase Realtime Database.
+ */
+function initApp() {
+  // Listen for auth state changes.
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      var email = user.email;
+      document.getElementById('quickstart-button').textContent = 'Sign out';
+      document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+      document.getElementById('user-detail').textContent = `Current user: ${email}`;
+    } else {
+      // Let's try to get a Google auth token programmatically.
+      document.getElementById('quickstart-button').textContent = 'Sign-in with Google';
+      document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+      document.getElementById('user-detail').textContent = '';
+    }
+    document.getElementById('quickstart-button').disabled = false;
+  });
+
+  document.getElementById('quickstart-button').addEventListener('click', startSignIn, false);
+}
+
+/**
+ * Start the auth flow and authorizes to Firebase.
+ * @param{boolean} interactive True if the OAuth flow should request with an interactive mode.
+ */
+async function startAuth(interactive){
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({
+    'login_hint': '186673725150-jnvblj3u6a3ndbed71go1t7l09nq3psl.apps.googleusercontent.com'
+  });
+  await firebase.auth().signInWithPopup(provider);
+}
+
+/**
+ * Starts the sign-in process.
+ */
+async function startSignIn() {
+  document.getElementById('quickstart-button').disabled = true;
+  if (firebase.auth().currentUser) {
+    firebase.auth().signOut();
+  } else {
+    await startAuth(true);
+  }
+}
+
+window.onload = function() {
+  initApp();
+};
