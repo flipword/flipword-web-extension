99f3a2b2ae86ba37e8ae19b572fb28ae8b4dfd8d
croquelois <croquelois.adrien@gmail.com>
2021-04-08 23:27:58 +0200

    :sparkles: auto fill native word in hoverPopup

--- hoverPopup.js
+++ hoverPopup.js
@@ -1,27 +1,34 @@
-var divContainer = document.createElement("div")
+var selection = window.getSelection().toString();
+console.log("selection:", selection)
+
 var inputNativeWord = document.createElement("input")
 var labelInputNativeWord = document.createElement("label")
-var inputForeignWord = document.createElement("input")
-var labelInputForeignWord = document.createElement("label")
-var submitButton = document.createElement("button")
-
 inputNativeWord.id = "flip-word-input-native-word"
 inputNativeWord.className = "flip-word-input-word"
 inputNativeWord.name = "flip-word-input-native-word"
 inputNativeWord.type = "text"
+inputNativeWord.value = selection
 labelInputNativeWord.htmlFor = "flip-word-input-native-word"
 labelInputNativeWord.innerText = "Native word:"
+
+var inputForeignWord = document.createElement("input")
+var labelInputForeignWord = document.createElement("label")
 inputForeignWord.id = "flip-word-input-foreign-word"
 inputForeignWord.name = "flip-word-input-foreign-word"
 inputForeignWord.className = "flip-word-input-word"
 inputForeignWord.type = "text"
 labelInputForeignWord.htmlFor = "flip-word-input-foreign-word"
 labelInputForeignWord.innerText = "Foreign word:"
+
+var submitButton = document.createElement("button")
 submitButton.innerText = "Submit"
+
+var divContainer = document.createElement("div")
 divContainer.id = "flip-word-hover-popup-container"
 divContainer.appendChild(labelInputNativeWord)
 divContainer.appendChild(inputNativeWord)
 divContainer.appendChild(labelInputForeignWord)
 divContainer.appendChild(inputForeignWord)
 divContainer.appendChild(submitButton)
+
 document.body.appendChild(divContainer)
