6a8585857e613e55d92ab7e9e8b5f3960162e01d
croquelois <croquelois.adrien@gmail.com>
2021-04-20 22:16:14 +0200

    :bug: fix adding multiple word by context menu

--- content-scripts/getSelectionAndOpenPopup.js
+++ content-scripts/getSelectionAndOpenPopup.js
@@ -1,3 +1,8 @@
-console.log("I'am getting selection");
-const selection = window.getSelection().toString();
-chrome.runtime.sendMessage({object: 'displayPopup', selection: selection});
+function sendDisplayPopupMessage(){
+    const selection = window.getSelection().toString();
+    chrome.runtime.sendMessage({object: 'displayPopup', selection: selection});
+}
+
+if(!!window.getSelection().toString()){
+    sendDisplayPopupMessage();
+}
